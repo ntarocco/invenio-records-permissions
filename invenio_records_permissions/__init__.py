a6d79bd06e8ba2707619f1cf3ba1ae054db33905
Nicola Tarocco <ntarocco@gmail.com>
2022-10-04 14:26:09 +0200

    release: v0.15.1

--- invenio_records_permissions/__init__.py
+++ invenio_records_permissions/__init__.py
@@ -155,7 +155,7 @@ The succinct encoding of the permissions for your instance gives you
 from .ext import InvenioRecordsPermissions
 from .policies import BasePermissionPolicy, RecordPermissionPolicy
 
-__version__ = "0.15.0"
+__version__ = "0.15.1"
 
 __all__ = (
     "__version__",
